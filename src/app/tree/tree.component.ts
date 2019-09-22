2b5e3b181ef4ac86010a62470b07e0514d79e8d2
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2019-09-23 10:27:20 +1200

    Use Tree Mock Data

---
+++ src/app/tree/tree.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Tree } from '../Tree';
+
+@Component({
+  selector: 'app-tree',
+  templateUrl: './tree.component.html',
+  styleUrls: ['./tree.component.css']
+})
+export class TreeComponent implements OnInit {
+
+  Trees: Tree[] = [];
+
+
+
+  constructor() {
+    for (let i = 0; i < 10; i++) {
+      let tree: Tree = {
+        id: i,
+        name: 'Tree ' + i,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
+        price: 100
+      };
+
+      this.Trees.push(tree);
+    }
+  }
+
+  ngOnInit() {
+  }
+
+}
