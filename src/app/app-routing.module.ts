91a5f769498c9776e1f9d28af130c953365f4b91
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2019-09-25 20:35:27 +1200

    Add tree route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TreeComponent } from './tree/tree.component';
 
 
 const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/tree', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
