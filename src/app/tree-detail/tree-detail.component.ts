fd9761cd62b0d116675da727e88369ac30bcdd27
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2019-09-25 20:36:12 +1200

    Add tree detail component

---
+++ src/app/tree-detail/tree-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Tree } from '../models/tree'
+
+@Component({
+  selector: 'app-tree-detail',
+  templateUrl: './tree-detail.component.html',
+  styleUrls: ['./tree-detail.component.css']
+})
+export class TreeDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
