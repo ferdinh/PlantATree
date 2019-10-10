ed8bd6ffe8b5055d9495edda4d858f3c135483ad
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2019-10-10 21:22:21 +1300

    Add Constructor to Tree

--- src/app/models/tree.ts
+++ src/app/models/tree.ts
@@ -3,4 +3,13 @@ export class Tree {
   name: string;
   price: number;
   description: string;
+  img_url: string;
+
+  constructor(id: number, name: string, price: number, description: string, img_url: string) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.description = description;
+    this.img_url = img_url;
+  }
 }
